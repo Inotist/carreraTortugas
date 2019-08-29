8c2ecf8144bc1227dfb4871b8c1317cf1bc20df4
Antonio Rabasco <43301727+Inotist@users.noreply.github.com>
2019-08-29 09:27:45 +0200

    Creacion del circuito con medidas y tortugas de colores

---
+++ main.py
@@ -0,0 +1,43 @@
+import turtle
+import math
+
+class Circuito:
+    corredores = []
+    
+    def __init__(self, width, height, *colors):
+        self.__startLine = int(-width/2 + width*0.05)
+        self.__finishLine = int(width/2 - width*0.05)
+        
+        self.__printScreen(width, height)
+        
+        if colors: self.__createRunners(colors)
+            
+    def __printScreen(self, width, height):
+        self.__screen = turtle.Screen()
+        self.__screen.setup(width, height)
+        self.__screen.bgcolor('lightgray')
+        self.__printLines(height)
+        
+    def __printLines(self, height):
+        for line in (self.__startLine, self.__finishLine):
+            new_printer = turtle.Turtle()
+            new_printer.penup()
+            new_printer.setpos(line, height/2)
+            new_printer.pendown()
+            new_printer.setpos(line, -height/2)
+        
+    def __createRunners(self, colors):
+        for i in range(len(colors)):
+            new_turtle = turtle.Turtle()
+            new_turtle.shape('turtle')
+            new_turtle.penup()
+            if i % 2 == 0: pos = i/2*20
+            else: pos = -math.ceil(i/2)*20
+            new_turtle.setpos(self.__startLine, pos)
+            new_turtle.color(colors[i])
+            
+            self.corredores.append(new_turtle)
+        
+        
+if __name__ == '__main__':
+    circuito = Circuito(640, 480, 'red', 'blue', 'green', 'orange')
